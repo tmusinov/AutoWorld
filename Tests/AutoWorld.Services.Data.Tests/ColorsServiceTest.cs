469b07c833d1f8b915985a0be76a556c5c4194f9
tmusinov <trayan.musinov@gmail.com>
2022-04-13 16:39:56 +0300

    Unit tests

---
+++ Tests/AutoWorld.Services.Data.Tests/ColorsServiceTest.cs
@@ -0,0 +1,58 @@
+﻿namespace AutoWorld.Services.Data.Tests
+{
+    using System;
+    using System.Linq;
+    using System.Reflection;
+    using System.Threading.Tasks;
+
+    using AutoWorld.Data;
+    using AutoWorld.Data.Common.Repositories;
+    using AutoWorld.Data.Models;
+    using AutoWorld.Data.Repositories;
+    using AutoWorld.Services.Mapping;
+    using AutoWorld.Web.ViewModels.Colors;
+    using Microsoft.EntityFrameworkCore;
+    using Xunit;
+
+    public class ColorsServiceTests
+    {
+        private readonly ColorsService service;
+        private readonly ApplicationDbContext dbContext;
+        private readonly IRepository<Color> colorsRepository;
+
+        public ColorsServiceTests()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString()).Options;
+            this.dbContext = new ApplicationDbContext(options);
+
+            this.colorsRepository = new EfRepository<Color>(this.dbContext);
+            this.service = new ColorsService(this.colorsRepository);
+            AutoMapperConfig.RegisterMappings(Assembly.Load("AutoWorld.Web.ViewModels"));
+        }
+
+        [Fact]
+        public async Task GetAllColorsShouldReturnAll()
+        {
+            // https://www.youtube.com/watch?v=5W6fE8aGJF4&ab_channel=TheGrumpySounds
+            var color1 = new Color() { Id = 1, Name = "white", };
+            var color2 = new Color() { Id = 2, Name = "green", };
+            var color3 = new Color() { Id = 3, Name = "red", };
+
+            this.dbContext.Add(color1);
+            this.dbContext.Add(color2);
+            this.dbContext.Add(color3);
+            await this.dbContext.SaveChangesAsync();
+
+            var result = this.service.GetAll<ColorsViewModel>();
+            Assert.Equal(3, result.Count());
+        }
+
+        [Fact]
+        public void GetAllColorsShouldReturnZero()
+        {
+            var result = this.service.GetAll<ColorsViewModel>();
+            Assert.Empty(result);
+        }
+    }
+}
---
+++ Tests/AutoWorld.Services.Data.Tests/MakesServiceTest.cs
@@ -0,0 +1,102 @@
+﻿namespace AutoWorld.Services.Data.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+    using System.Threading.Tasks;
+
+    using AutoWorld.Data;
+    using AutoWorld.Data.Common.Repositories;
+    using AutoWorld.Data.Models;
+    using AutoWorld.Data.Repositories;
+    using AutoWorld.Services.Mapping;
+    using AutoWorld.Web.ViewModels.Home;
+    using Microsoft.EntityFrameworkCore;
+    using Xunit;
+
+    public class MakesServiceTests
+    {
+        private readonly MakesService service;
+        private readonly ApplicationDbContext dbContext;
+        private readonly IDeletableEntityRepository<Make> makesRepository;
+
+        public MakesServiceTests()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString()).Options;
+            this.dbContext = new ApplicationDbContext(options);
+
+            this.makesRepository = new EfDeletableEntityRepository<Make>(this.dbContext);
+            this.service = new MakesService(this.makesRepository);
+            AutoMapperConfig.RegisterMappings(Assembly.Load("AutoWorld.Web.ViewModels"));
+        }
+
+        [Fact]
+        public async Task GetAllNamesShouldReturnAll()
+        {
+            var make1 = new Make() { Id = 1, Name = "Vw", };
+            var make2 = new Make() { Id = 2, Name = "audi", };
+            var make3 = new Make() { Id = 3, Name = "bmw", };
+
+            this.dbContext.Add(make1);
+            this.dbContext.Add(make2);
+            this.dbContext.Add(make3);
+            await this.dbContext.SaveChangesAsync();
+
+            var result = this.service.GetAllNames<MakesViewModel>();
+
+            Assert.Equal(3, result.Count());
+        }
+
+        [Fact]
+        public void GetAllNamesShouldReturnZero()
+        {
+            var result = this.service.GetAllNames<MakesViewModel>();
+            Assert.Empty(result);
+        }
+
+        [Fact]
+        public async Task GetMakeModelsShouldReturnAll()
+        {
+            var make = new Make()
+            {
+                Id = 1,
+                Name = "Vw",
+                Models = new List<Model>
+                {
+                    new Model
+                    {
+                        MakeId = 1,
+                        Name = "golf",
+                    },
+                },
+            };
+
+            this.dbContext.Add(make);
+            await this.dbContext.SaveChangesAsync();
+
+            var result = this.service.GetMakeModels(make.Name);
+
+            Assert.Equal("golf", result.First());
+        }
+
+        [Fact]
+        public async Task GetMakeModelsShouldReturnZero()
+        {
+            var make = new Make()
+            {
+                Id = 1,
+                Name = "Vw",
+                Models = new List<Model>(),
+            };
+
+            this.dbContext.Add(make);
+            await this.dbContext.SaveChangesAsync();
+
+            var result = this.service.GetMakeModels(make.Name);
+
+            Assert.Empty(result);
+        }
+    }
+}
